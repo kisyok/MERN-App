2a997351cc3100c4b1c628e35139812b76ccc5ae
Kisyok Indran Veerasamy <kisoindran@gmail.com>
2023-02-22 20:07:02 +0800

    added form

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const WorkoutForm = () => {
+    const [title, setTitle] = useState('')
+    const [load, setLoad] = useState('')
+    const [reps, setReps] = useState('')
+    const [error, setError] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        const workout = { title, load, reps }
+        const res = await fetch('/api/workouts/', {
+            method: 'POST',
+            body: JSON.stringify(workout),
+            headers: {
+                'Content-Type': "application/json"
+            }
+        })
+
+        const json = await res.json()
+
+        if(!res.ok) {
+            setError(json.msg)
+        }
+
+        if(res.ok) {
+            setError(null)
+            setTitle('')
+            setLoad('')
+            setReps('')
+            console.log("New workout added")
+        }
+    }
+
+    return (
+        <form className="create" onSubmit={handleSubmit}>
+            <h3>Add New Workout</h3>
+            <label>Exercise Title</label>
+            <input 
+                type="text"
+                onChange={((e) => setTitle(e.target.value))}
+                value={title}
+            />
+            <label>Load (kg)</label>
+            <input 
+                type="number"
+                onChange={((e) => setLoad(e.target.value))}
+                value={load}
+            />
+            <label>Reps</label>
+            <input 
+                type="text"
+                onChange={((e) => setReps(e.target.value))}
+                value={reps}
+            />
+            <button>Add Workout</button>
+            {error && <div className="error">{ error }</div> }
+        </form>    
+    )
+}
+
+export default WorkoutForm
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
 
 const Home = () => {
 
@@ -22,9 +23,10 @@ const Home = () => {
         <div className="home">
             <div className="workouts">
                 { workouts && workouts.map((workout) => (
-                    <WorkoutDetails key="workout._id" workout={workout} />
+                    <WorkoutDetails key={workout._id} workout={workout} />
                 ))}
             </div>
+            <WorkoutForm />
         </div>
     )
 }
