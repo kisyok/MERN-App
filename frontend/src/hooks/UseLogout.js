2de85e98c1f5e891d8bdc7e6266ae9c37157de4b
Kisyok Indran Veerasamy <kisoindran@gmail.com>
2023-02-23 20:39:12 +0800

    minor updates

--- frontend/src/hooks/UseLogout.js
+++ frontend/src/hooks/UseLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from './UseAuthContext'
+import { useWorkoutsContext } from './UseWorkoutsContext'
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext()
+  const { dispatch: workoutsDispatch } = useWorkoutsContext()
 
   const logout = () => {
     // remove user from storage
@@ -9,6 +11,7 @@ export const useLogout = () => {
 
     // dispatch logout action
     dispatch({ type: 'LOGOUT' })
+    workoutsDispatch({ type: 'SET_WORKOUTS', payload: null })
   }
 
   return { logout }
