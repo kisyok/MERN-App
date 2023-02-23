b5bf719f58415d2c10050229074a80fdda715abc
Kisyok Indran Veerasamy <kisoindran@gmail.com>
2023-02-23 18:30:09 +0800

    login logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,16 @@ const jwt = require('jsonwebtoken')
 
 // login
 const loginUser = async (req, res) => {
-    res.json({ mg: "login user" })
+    const { email, password } = req.body
+
+    try {
+        const user = await User.login(email, password)
+        // create token
+        const token = createToken(user._id)
+        res.status(200).json({ email, token })
+    } catch (err) {
+        res.status(400).json({ err: err.message})
+    }
 }
 
 // signup
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const bcryot = require('bcrypt')
+const bcrypt = require('bcrypt')
 const validator = require('validator')
 
 const Schema = mongoose.Schema
@@ -38,10 +38,30 @@ userSchema.statics.signup = async function (email, password) {
         throw Error("Email already in use")
     }
 
-    const salt = await bcryot.genSalt(10)
-    const hash = await bcryot.hash(password, salt)
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
 
     const user = await this.create({ email, password: hash })
+
+    return user
+}
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+    if (!email || !password) {
+        throw Error("All fields must be filled")
+    }
+
+    const user = await this.findOne({ email })
+    if (!user) {
+        throw Error("Email does not exist!")
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error("Incorrect password")
+    }
 
     return user
 }
